578d073e588e163a56ac2577e708ffae62914136
Wonjin <wonjin.lee@student.kuleuven.be>
2026-04-18 18:37:45 +0200

    Added CI/CD pipeline

---
+++ math_app.py
@@ -0,0 +1,6 @@
+# math_app.py
+def add_numbers(a, b):
+    return a + b
+
+def subtract_numbers(a, b):
+    return a - b
---
+++ test_math_app.py
@@ -0,0 +1,6 @@
+# test_math_app.py
+from math_app import add_numbers
+
+def test_addition():
+    assert add_numbers(2, 3) == 5
+    assert add_numbers(-1, 1) == 0
