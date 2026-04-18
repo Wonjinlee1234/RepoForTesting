63aa107c7cdcbc53a9b4111120658055bdb0cf53
Wonjin <wonjin.lee@student.kuleuven.be>
2026-04-18 18:52:47 +0200

    Added CI/CD pipeline

--- test_math_app.py
+++ test_math_app.py
@@ -2,5 +2,5 @@
 from math_app import add_numbers
 
 def test_addition():
-    assert add_numbers(2, 3) == 0
-    assert add_numbers(-1, 1) == 1
+    assert add_numbers(2, 3) == 5
+    assert add_numbers(-1, 1) == 0
